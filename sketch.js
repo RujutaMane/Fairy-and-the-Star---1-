5fe58ad0b4616ccaed9dcec3041a78d4e02cacb6
RujutaMane <76509583+RujutaMane@users.noreply.github.com>
2021-12-14 13:16:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,62 +1,30 @@
-var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
-var star, starBody;
+var canvas, backgroundImage;
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
 
-function preload(){
-	starImg = loadImage("images/star.png");
-	fairyImg = loadAnimation("images/fairy1.png","images/fairy2.png");
-	bgImg = loadImage("images/starNight.png");
-	fairyVoice = loadSound("sound/JoyMusic.mp3");
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
 }
 
-function setup() {
-	createCanvas(800, 750);
 
-	fairyVoice.play();
-
-	fairy = createSprite(130, 520);
-	fairy.addAnimation("fairyflying",fairyImg);  
-	fairy.scale = 0.25;
-
-	star = createSprite(650,30);
-	star.addImage(starImg);
-	star.scale = 0.2;
-
-	engine = Engine.create();
-	world = engine.world;
-
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:false});
-	World.add(world, starBody);
-	
-	Engine.run(engine);
-
-}
-
-function draw() {
-  background(bgImg);
-  
-  keyPressed();
-  if(star.position.y > 470){
-	star.y = 470;
-	fairy.y = 500;
-	fairy.x = star.x - 140;
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
   }
-
-  drawSprites();
-
-}
-
-function keyPressed() {
-	if(keyDown(LEFT_ARROW)){
-		fairy.x -= 5;
-	} else if(keyDown(RIGHT_ARROW)){
-		fairy.x += 5;
-	} else if(keyDown(DOWN_ARROW)){
-		star.velocityY = 15;
-	}
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
 }
